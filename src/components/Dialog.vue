2a25cdc91d7b13519e930f37cb6b81830091100d
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-12-28 18:36:21 -0600

    Creating the Dialog component

---
+++ src/components/Dialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-dialog v-model="dialog" max-width="290">
+    <template v-slot:activator="{ on }">
+      <div v-on="on">
+        <slot name="activator" />
+      </div>
+    </template>
+    <v-card>
+      <v-card-title class="headline">
+        <slot name="title" />
+      </v-card-title>
+      <v-card-text>
+        <slot />
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <div @click="dialog = false">
+          <slot name="actions" />
+        </div>
+        <!-- <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+          <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn> -->
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- src/components/MyFeedStocksList.vue
+++ src/components/MyFeedStocksList.vue
@@ -6,25 +6,39 @@
     <v-card v-else>
       <v-list>
         <v-list-item-group>
-          <v-list-item
-            v-for="feedstock in myFeedstocks"
-            :key="feedstock.id"
-            @click="
-              $router.push({
-                name: 'My feedstock',
-                params: {
-                  id: feedstock.id
-                }
-              })
-            "
-          >
-            <v-list-item-content>
+          <v-list-item v-for="feedstock in myFeedstocks" :key="feedstock.id">
+            <v-list-item-content
+              @click="
+                $router.push({
+                  name: 'My feedstock',
+                  params: {
+                    id: feedstock.id
+                  }
+                })
+              "
+            >
               <v-list-item-title
                 v-text="feedstock.material"
               ></v-list-item-title>
             </v-list-item-content>
             <v-list-item-icon>
-              <v-icon>mdi-delete</v-icon>
+              <Dialog>
+                <template slot="activator">
+                  <v-btn text icon>
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </template>
+                <template slot="title">
+                  Delete
+                </template>
+                <template>
+                  ¿Do you want to delete the feedstock {{ feedstock.material }}?
+                </template>
+                <template slot="actions">
+                  <v-btn text @click="dialog = false">Cancel</v-btn>
+                  <v-btn color="red" dark @click="dialog = false">Delete</v-btn>
+                </template>
+              </Dialog>
             </v-list-item-icon>
           </v-list-item>
         </v-list-item-group>
@@ -52,8 +66,12 @@
 
 <script>
 import { mapState } from 'vuex'
+import Dialog from '@/components/Dialog'
 
 export default {
+  components: {
+    Dialog
+  },
   computed: {
     ...mapState('feedstocks', ['myFeedstocks'])
   }

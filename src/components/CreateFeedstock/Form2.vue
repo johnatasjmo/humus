6982bcf60bca9b7383ef358955e344ea0bf7a211
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2020-01-12 12:12:03 -0600

    Validation fixes

--- src/components/CreateFeedstock/Form2.vue
+++ src/components/CreateFeedstock/Form2.vue
@@ -7,9 +7,9 @@
             v-model="formValues.nitrogen"
             v-mask="formMasks.twoDigitsTwoDecimals"
             :rules="validations.nitrogenRules"
-            type="number"
             inputmode="numeric"
             label="Nitrogen % (dry weight)"
+            placeholder="00.00"
             required
           ></v-text-field>
         </v-col>
@@ -18,9 +18,9 @@
             v-model="formValues.carbon"
             v-mask="formMasks.twoDigitsTwoDecimals"
             :rules="validations.carbonRules"
-            type="number"
             inputmode="numeric"
             label="Carbon % (dry weight)"
+            placeholder="00.00"
             required
           ></v-text-field>
         </v-col>
@@ -38,9 +38,9 @@
             v-model="formValues.moisture_content"
             v-mask="formMasks.twoDigitsOneDecimal"
             :rules="validations.moistureContentRules"
-            type="number"
             inputmode="numeric"
             label="Moisture content (weight to weight)"
+            placeholder="00.0"
             required
           ></v-text-field>
         </v-col>
@@ -76,44 +76,25 @@ export default {
         twoDigitsOneDecimal: '##.#'
       },
       formValues: {
-        nitrogen: '0000',
-        carbon: '0000',
+        nitrogen: null,
+        carbon: null,
         cn_ratio: null,
-        moisture_content: '000',
+        moisture_content: null,
         bulk_density_yd: null
       },
       validations: {
-        nitrogenRules: [
-          v => !!v || 'Nitrogen is required',
-          v =>
-            (!!v && !!v.match(/^-?\d*(\.\d+)?$/)) ||
-            'Value should be a valid number',
-          v => (v && v >= 0) || 'Nitrogen cannot be negative',
-          v => (v && v <= 100) || 'Nitrogen must be less than 100'
-        ],
-        carbonRules: [
-          v => !!v || 'Carbon is required',
-          v =>
-            (!!v && !!v.match(/^-?\d*(\.\d+)?$/)) ||
-            'Value should be a valid number',
-          v => (v && v >= 0) || 'Carbon cannot be negative',
-          v => (v && v <= 100) || 'Carbon must be less than 100'
-        ],
+        nitrogenRules: [v => !!v || 'Nitrogen is required'],
+        carbonRules: [v => !!v || 'Carbon is required'],
         cnRatioRules: [
           v => !!v || 'C:N ratio is required',
           v => /^-?\d*(\.\d+)?$/.test(v) || 'C:N ratio must be a valid number',
           v => (v && v <= 1500) || 'C:N ratio must be less than 1500'
         ],
-        moistureContentRules: [
-          v => !!v || 'Moisture content is required',
-          v =>
-            (!!v && !!v.match(/^-?\d*(\.\d+)?$/)) ||
-            'Value should be a valid number',
-          v => (v && v <= 100) || 'Name must be less than 10 characters'
-        ],
+        moistureContentRules: [v => !!v || 'Moisture content is required'],
         bulkDensityRules: [
           v => !!v || 'Bulk density is required',
-          v => /^-?\d*(\.\d+)?$/.test(v) || 'Value must be a valid number',
+          v =>
+            /^-?\d*(\.\d+)?$/.test(v) || 'Bulk density must be a valid number',
           v => (v && v.length <= 100) || 'Name must be less than 10 characters'
         ]
       }

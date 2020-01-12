9914fcc5fd04a8b372f42c9e157d18212f962854
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2020-01-11 18:43:50 -0600

    feedstock list item layout with more details

--- src/components/FeedstocksByCategoryList.vue
+++ src/components/FeedstocksByCategoryList.vue
@@ -17,8 +17,28 @@
           >
             <v-list-item-content>
               <v-list-item-title
+                class="font-weight-medium"
                 v-text="feedstock.material"
               ></v-list-item-title>
+              <v-container class="pa-0">
+                <v-row justify="start">
+                  <v-col cols="4" class="caption pt-1 pb-0">
+                    N = {{ feedstock.nitrogen }}%
+                  </v-col>
+                  <v-col cols="4" class="caption pt-1 pb-0">
+                    C = {{ feedstock.carbon }}%
+                  </v-col>
+                  <v-col cols="4" class="caption pt-1 pb-0">
+                    CN = {{ feedstock.cn_ratio }}
+                  </v-col>
+                  <v-col cols="4" class="caption pt-1 pb-0">
+                    Moisture = {{ feedstock.moisture_content }}%
+                  </v-col>
+                  <v-col cols="4" class="caption pt-1 pb-0">
+                    Density = {{ feedstock.bulk_density_yd }} lb/cy
+                  </v-col>
+                </v-row>
+              </v-container>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>

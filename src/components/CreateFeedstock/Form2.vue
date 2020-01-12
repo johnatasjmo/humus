4a0ff1fbc377c0c5388afec602d4a95571c85592
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2020-01-12 11:56:56 -0600

    Adding validations ins feedstock create form2

--- src/components/CreateFeedstock/Form2.vue
+++ src/components/CreateFeedstock/Form2.vue
@@ -5,7 +5,10 @@
         <v-col cols="12" md="6">
           <v-text-field
             v-model="formValues.nitrogen"
+            v-mask="formMasks.twoDigitsTwoDecimals"
             :rules="validations.nitrogenRules"
+            type="number"
+            inputmode="numeric"
             label="Nitrogen % (dry weight)"
             required
           ></v-text-field>
@@ -13,7 +16,10 @@
         <v-col cols="12" md="6">
           <v-text-field
             v-model="formValues.carbon"
+            v-mask="formMasks.twoDigitsTwoDecimals"
             :rules="validations.carbonRules"
+            type="number"
+            inputmode="numeric"
             label="Carbon % (dry weight)"
             required
           ></v-text-field>
@@ -22,6 +28,7 @@
           <v-text-field
             v-model="formValues.cn_ratio"
             :rules="validations.cnRatioRules"
+            inputmode="numeric"
             label="C:N ratio (weight to weight)"
             required
           ></v-text-field>
@@ -29,7 +36,10 @@
         <v-col cols="12" md="6">
           <v-text-field
             v-model="formValues.moisture_content"
+            v-mask="formMasks.twoDigitsOneDecimal"
             :rules="validations.moistureContentRules"
+            type="number"
+            inputmode="numeric"
             label="Moisture content (weight to weight)"
             required
           ></v-text-field>
@@ -38,7 +48,8 @@
           <v-text-field
             v-model="formValues.bulk_density_yd"
             :rules="validations.bulkDensityRules"
-            label="Bulk density (pound per cubic)"
+            inputmode="numeric"
+            label="Bulk density (lb/cy)"
             required
           ></v-text-field>
         </v-col>
@@ -51,15 +62,24 @@
 </template>
 
 <script>
+import { mask } from 'vue-the-mask'
+
 export default {
+  directives: {
+    mask
+  },
   data() {
     return {
       valid: false,
+      formMasks: {
+        twoDigitsTwoDecimals: '##.##',
+        twoDigitsOneDecimal: '##.#'
+      },
       formValues: {
-        nitrogen: null,
-        carbon: null,
+        nitrogen: '0000',
+        carbon: '0000',
         cn_ratio: null,
-        moisture_content: null,
+        moisture_content: '000',
         bulk_density_yd: null
       },
       validations: {
@@ -67,31 +87,34 @@ export default {
           v => !!v || 'Nitrogen is required',
           v =>
             (!!v && !!v.match(/^-?\d*(\.\d+)?$/)) ||
-            'Value should be a valid number'
+            'Value should be a valid number',
+          v => (v && v >= 0) || 'Nitrogen cannot be negative',
+          v => (v && v <= 100) || 'Nitrogen must be less than 100'
         ],
         carbonRules: [
           v => !!v || 'Carbon is required',
           v =>
             (!!v && !!v.match(/^-?\d*(\.\d+)?$/)) ||
-            'Value should be a valid number'
+            'Value should be a valid number',
+          v => (v && v >= 0) || 'Carbon cannot be negative',
+          v => (v && v <= 100) || 'Carbon must be less than 100'
         ],
         cnRatioRules: [
           v => !!v || 'C:N ratio is required',
-          v =>
-            (!!v && !!v.match(/^-?\d*(\.\d+)?$/)) ||
-            'Value should be a valid number'
+          v => /^-?\d*(\.\d+)?$/.test(v) || 'C:N ratio must be a valid number',
+          v => (v && v <= 1500) || 'C:N ratio must be less than 1500'
         ],
         moistureContentRules: [
           v => !!v || 'Moisture content is required',
           v =>
             (!!v && !!v.match(/^-?\d*(\.\d+)?$/)) ||
-            'Value should be a valid number'
+            'Value should be a valid number',
+          v => (v && v <= 100) || 'Name must be less than 10 characters'
         ],
         bulkDensityRules: [
           v => !!v || 'Bulk density is required',
-          v =>
-            (!!v && !!v.match(/^-?\d*(\.\d+)?$/)) ||
-            'Value should be a valid number'
+          v => /^-?\d*(\.\d+)?$/.test(v) || 'Value must be a valid number',
+          v => (v && v.length <= 100) || 'Name must be less than 10 characters'
         ]
       }
     }

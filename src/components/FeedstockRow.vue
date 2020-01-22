3383d370374e3ff5220cf8116fa94b61c3c0aedf
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2020-01-21 20:44:03 -0600

    changing font size and stretch in feedstockRow

--- src/components/FeedstockRow.vue
+++ src/components/FeedstockRow.vue
@@ -4,25 +4,41 @@
       class="font-weight-medium"
       v-text="feedstock.material"
     ></v-list-item-title>
-    <v-container class="pa-0">
+    <div class="flexContainer mt-1">
+      <p class="flexItem2 customCondensed mb-2">
+        N = {{ feedstock.nitrogen }}%
+      </p>
+      <p class="flexItem2 customCondensed mb-2">C = {{ feedstock.carbon }}%</p>
+      <p class="flexItem1 customCondensed mb-2">
+        CN = {{ feedstock.cn_ratio }}
+      </p>
+      <p class="flexItem2 customCondensed mb-2">
+        BD = {{ feedstock.bulk_density_yd }} lb/cy
+      </p>
+      <p class="flexItem2 customCondensed mb-2">
+        Moisture = {{ feedstock.moisture_content }}%
+      </p>
+      <p class="flexItem1 customCondensed mb-2"></p>
+    </div>
+    <!-- <v-container class="pa-0">
       <v-row justify="start">
-        <v-col cols="4" class="caption pt-1 pb-0 pr-0">
+        <v-col cols="4" class="customCondensed pt-1 pb-0 pr-0">
           N = {{ feedstock.nitrogen }}%
         </v-col>
-        <v-col cols="4" class="caption pt-1 pb-0 pr-0">
+        <v-col cols="4" class="customCondensed pt-1 pb-0 pr-0">
           C = {{ feedstock.carbon }}%
         </v-col>
-        <v-col cols="4" class="caption pt-1 pb-0 pr-0">
+        <v-col cols="4" class="customCondensed pt-1 pb-0 pr-0">
           CN = {{ feedstock.cn_ratio }}
         </v-col>
-        <v-col cols="4" class="caption pt-1 pb-0 pr-0">
+        <v-col cols="4" class="customCondensed pt-1 pb-0 pr-0">
           BD = {{ feedstock.bulk_density_yd }} lb/cy
         </v-col>
-        <v-col cols="4" class="caption pt-1 pb-0 pr-0">
+        <v-col cols="4" class="customCondensed pt-1 pb-0 pr-0">
           Moisture = {{ feedstock.moisture_content }}%
         </v-col>
       </v-row>
-    </v-container>
+    </v-container> -->
   </v-list-item-content>
 </template>
 
@@ -36,3 +52,24 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.customCondensed {
+  font-family: Roboto !important;
+  font-size: 0.74em !important;
+  /* font-weight: 200 !important; */
+  font-stretch: condensed !important;
+}
+.flexContainer {
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+}
+.flexItem1 {
+  flex: 1 0 20%;
+}
+.flexItem2 {
+  flex: 2 0 35%;
+  margin-right: 5px;
+}
+</style>

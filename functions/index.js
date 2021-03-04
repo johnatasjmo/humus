111fdc0463e7b89d8fd1515a1b0082b0e955ed29
Johnatas Montezuma <jmo@logisticsmaven.com>
2021-03-03 20:18:13 -0800

    function modified for backup into bucket

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
-const functions = require("firebase-functions");
+// // this function makes a backup of wanu into google cloud - jmo 2021-03-03
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const firestore = require('@google-cloud/firestore');
+const client = new firestore.v1.FirestoreAdminClient();
+
+// Replace BUCKET_NAME
+const bucket = 'gs://humus-backup';
+
+exports.scheduledFirestoreExport = functions.pubsub
+                                            .schedule('every 24 hours')
+                                            .onRun((context) => {
+
+  const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+  const databaseName = 
+    client.databasePath(projectId, '(default)');
+
+  return client.exportDocuments({
+    name: databaseName,
+    outputUriPrefix: bucket,
+    // Leave collectionIds empty to export all collections
+    // or set to a list of collection IDs to export,
+    // collectionIds: ['users', 'posts']
+    collectionIds: []
+    })
+  .then(responses => {
+    const response = responses[0];
+    console.log(`Operation Name: ${response['name']}`);
+  })
+  .catch(err => {
+    console.error(err);
+    throw new Error('Export operation failed');
+  });
+});
+
